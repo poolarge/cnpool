a387125176c5da0dfdcee5e0f7c81cce581d3de9
small <small@poolarge.com>
2021-12-14 09:17:51 +0000

    modify monero_root in git clone monero in the file of compile-cnpool.sh

--- compile-cnpool.sh
+++ compile-cnpool.sh
@@ -1,8 +1,8 @@
 #！bin/bash
 # compile xmr
 sudo apt install -y build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev ccache doxygen graphviz
-git clone --recursive https://github.com/monero-project/monero
-cd monero
+git clone --recursive https://github.com/monero-project/monero ~/monero
+cd ~/monero
 git checkout 992b7ce30fa101f55314b89aa5bdf20881f5b43d
 git submodule sync && git submodule update
 make
